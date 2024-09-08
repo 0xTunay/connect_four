537ad5a8f0fd7ee13a68dc56cc2d857db8eff85a
Ttunay <tigrus232323k@gmail.com>
2024-09-08 19:08:16 +0300

    add new function and choice

---
+++ src/playrs.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../include/playrs.h"
+
+void name_playrs(info *Info_Playrsl)
+{
+
+    Info_Playrsl = malloc(sizeof(info));
+
+    if(Info_Playrsl == NULL){
+        perror("Memory error");
+        exit(1);
+    }
+
+    puts("Enter first player");
+    scanf("%31s",Info_Playrsl->first_player);
+
+    puts("Enter second playr");
+    scanf("%31s",Info_Playrsl->seconect_playr);
+
+    choice_symbol(Info_Playrsl);
+}
+
+void choice_symbol(info *Info_Playrsl)
+{
+    int x;
+    
+    printf("%s\n %s\n",Info_Playrsl->first_player,Info_Playrsl->seconect_playr);
+
+    puts("Who choice? 1 or 2");
+    scanf("%d",&x);
+
+    if(x == 1){
+    printf("Choice symbol, * or 0 %s",Info_Playrsl->first_player);
+    scanf("%s",Info_Playrsl->symbol_for_first);
+
+    if(strcmp(Info_Playrsl->first_player,"*")== 0)
+        strcpy(Info_Playrsl->seconect_playr,"0");
+    } else {
+        printf("Choice symbol, * or 0 %s",Info_Playrsl->seconect_playr);
+    scanf("%s",Info_Playrsl->seconect_playr);
+
+    if(strcmp(Info_Playrsl->seconect_playr,"*")== 0)
+        strcpy(Info_Playrsl->first_player,"0");
+    }
+}
